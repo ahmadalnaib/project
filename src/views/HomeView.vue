b00721ffdc0a34136a543866e243703fe400b559
ahmadalnaib <alnaib888@gmail.com>
2024-01-01 14:58:08 +0100

    fetch the dAta

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup>
+<script >
+export default{
+  name:'Home',
+  components:{},
+  data(){
+    return {
+      projects:[]
+    }
+
+  },
+
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(response => response.json())
+    .then(data => this.projects = data)
+    .catch(error => console.log(error))
+  }
+
+}
 
 </script>
 
 <template>
   <main>
-   Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+         <p>{{ project.title }}</p>
+      </div>
+    </div>
+
+      
   </main>
 </template>
